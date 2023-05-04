e3e200110feb4ca619a2b4515a5bef97526d7a10
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-04 13:07:54 -0300

    parte 2

--- portfolio/Relogio Digital/script.js
+++ portfolio/Relogio Digital/script.js
@@ -8,6 +8,12 @@ const relogio = setIntervale(function time(){
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
 
+    if (hr < 10) hr = '0' + hr; 
+
+    if(min < 10) min = '0' + min; 
+
+    if (s < 10) hr = '0' + hr
+
     horas.textContet = hr; 
     minutos.textCentent = min; 
     segundos.textContent = s;
