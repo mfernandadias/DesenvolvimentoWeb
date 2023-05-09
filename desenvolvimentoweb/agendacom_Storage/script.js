f8bafb96095f11f318c1bbc90916e994460df343
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-09 05:03:34 -0300

    parte2

--- desenvolvimentoweb/agendacom_Storage/script.js
+++ desenvolvimentoweb/agendacom_Storage/script.js
@@ -1,1 +1,39 @@
-localStorage
+let lista = localStorage.getItem("minhalista");
+const formulario = document.querySelector("form");
+const ulPessoas = document.querySelector("ul");
+
+if(lista) {
+    lista = JSON.parse(lista);
+} else {
+    lista = [];
+}
+
+listar();
+
+formulario.addEventListener("submil", function (e){
+    e.preventDefault();
+    let novaPessoa = new object();
+    novaPessoa.nome = this.nome.value; 
+    novaPessoa.telefone = this.nome.value; 
+    novaPessoa.telefone = this.telefone.value; 
+    console.log(novaPessoa);
+
+    lista.push(novaPessoa);
+
+    this.reset();
+
+    localStorage.setItem("minhaLista", JSON.stringify(lista));
+
+    listar();
+})
+
+function listar() {
+    ulPessoas.innerHTML = "";
+    lista.forEach((item, key) => {
+        console.log(item,key);
+    });
+}
+
+
+
+
