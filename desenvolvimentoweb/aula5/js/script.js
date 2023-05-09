0d2a69a767838b4df7f3c7752e2a49272513f040
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-09 01:54:23 -0300

    parte3
    javascript feito

--- desenvolvimentoweb/aula5/js/script.js
+++ desenvolvimentoweb/aula5/js/script.js
@@ -1,5 +1,47 @@
 
-function meuescopo () {
+const form = document.querySelector('.form');
+const resultado = document.querySelector('.resultado');
+
+function mostrarResultado() {
+    const login = form.querySelector('.login').value;
+    const nome = form.querySelector('.nome').value; 
+    const sobrenome = form.querySelector('.sobrenome').value;
+    const n1 = form.querySelector('.n1').value;
+    const n2 = form.querySelector('.n2').value;
+    const nota_final = form.querySelector('.Nota_Final').value; 
+     
+
+    const linha = document.createElement('tr');
+    linha.innerHTML = `
+    <td>${login}</td>
+    <td>${nome}</td>
+    <td>${sobrenome}</td>
+    <td>${n1}</td>
+    <td>${n2}</td>
+    <td>${nota_final}</td>
+    `;
+
+    const tbody = resultado.querySelector('tbody');
+    tbody.appendChild(linha);
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    mostrarResultado();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*function meuescopo () {
    const form = document.querySelector('.form');
    const resultado = document.querySelector('.resultado');
 
@@ -15,9 +57,22 @@ function meuescopo () {
     const N2 = form.querySelector('.N2');
     const Nota_Final = form.querySelector('.Nota_Final');
 
-    console.log(login, nome, sobrenome, N1, N2, Nota_Final);
+   pessoas.push ({
+    login: login.value, 
+    nome: nome.nodeValue, 
+    sobrenome: sobrenome.value,
+    N1: N1.value,
+    N2: N2.value, 
+    Nota_Final: Nota_Final.value
+   });
+   
+   console.log(pessoas);
+
+   resultado.innerHTML += `<p>${login.value} ${nome.value} 
+   ${sobrenome.value} ${N1.value} ${N2.value} ${Nota_Final.value}</p>`
+   
    }
 
    form.addEventListener('submit', recebeEventForm);
 }
-meuescopo();
+meuescopo(); */
