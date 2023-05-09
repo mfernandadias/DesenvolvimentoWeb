cd90cd3edddfa311c196a2a9bcc9685dfafe65a8
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-08 22:13:41 -0300

    parte2

--- desenvolvimentoweb/aula5/js/script.js
+++ desenvolvimentoweb/aula5/js/script.js
@@ -1,3 +1,23 @@
+
 function meuescopo () {
-    let nome = 'Maria';
+   const form = document.querySelector('.form');
+   const resultado = document.querySelector('.resultado');
+
+   const pessoas = [];
+   
+   function recebeEventForm (evento) {
+    evento.preventDefault();
+
+    const login = form.querySelector('.login');
+    const nome = form.querySelector('.nome');
+    const sobrenome = form.querySelector('.sobrenome');
+    const N1 = form.querySelector('.N1');
+    const N2 = form.querySelector('.N2');
+    const Nota_Final = form.querySelector('.Nota_Final');
+
+    console.log(login, nome, sobrenome, N1, N2, Nota_Final);
+   }
+
+   form.addEventListener('submit', recebeEventForm);
 }
+meuescopo();
