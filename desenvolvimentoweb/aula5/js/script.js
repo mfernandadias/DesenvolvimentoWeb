db9fc696da579763514b48c1dee9ea23405151a7
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-12 03:09:35 -0300

    parte 8
    não esta entrando os dados dos estudantes

--- desenvolvimentoweb/aula5/js/script.js
+++ desenvolvimentoweb/aula5/js/script.js
@@ -5,14 +5,14 @@ const resultado = document.querySelector('.resultado');
 function mostrarResultado() {
     const nome = form.querySelector('.nome').value; 
     const sobrenome = form.querySelector('.sobrenome').value;
-    const instituicao_de_ensino = form.querySelector('.Instiuicao_de_ensino').value;
+    const Instituicao_de_ensino = form.querySelector('.Instiuicao_de_ensino').value;
     const cargo = form.querySelector('.cargo').value;
 
     const linha = document.createElement('tr');
     linha.innerHTML = `
     <td>${nome}</td>
     <td>${sobrenome}</td>
-    <td>${instituicao_de_ensino}</td>
+    <td>${Instituicao_de_ensino}</td>
     <td>${cargo}</td>
     `;
 
