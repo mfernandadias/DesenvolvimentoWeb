140708fd4a469a6d3a8faf49568d1db0a860cc57
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-12 02:46:13 -0300

    parte 7
    arrumei o javascript

--- desenvolvimentoweb/aula5/js/script.js
+++ desenvolvimentoweb/aula5/js/script.js
@@ -3,22 +3,17 @@ const form = document.querySelector('.form');
 const resultado = document.querySelector('.resultado');
 
 function mostrarResultado() {
-    const login = form.querySelector('.Login').value;
     const nome = form.querySelector('.nome').value; 
     const sobrenome = form.querySelector('.sobrenome').value;
-    const n1 = form.querySelector('.n1').value;
-    const n2 = form.querySelector('.n2').value;
-    const nota_final = form.querySelector('.Nota_Final').value; 
-     
+    const instituicao_de_ensino = form.querySelector('.Instiuicao_de_ensino').value;
+    const cargo = form.querySelector('.cargo').value;
 
     const linha = document.createElement('tr');
     linha.innerHTML = `
-    <td>${login}</td>
     <td>${nome}</td>
     <td>${sobrenome}</td>
-    <td>${n1}</td>
-    <td>${n2}</td>
-    <td>${nota_final}</td>
+    <td>${instituicao_de_ensino}</td>
+    <td>${cargo}</td>
     `;
 
     const tbody = resultado.querySelector('tbody');
@@ -29,14 +24,3 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     mostrarResultado();
 });
-
-
-
-
-
-
-
-
-
-
-
