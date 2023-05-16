a21e6dddd2b0f8ac29555cb37009f9c15c4aa864
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-05-15 21:23:36 -0300

    final

--- desenvolvimentoweb/napraticaHTML_CSS_JavaScript/devConverter/script.js
+++ desenvolvimentoweb/napraticaHTML_CSS_JavaScript/devConverter/script.js
@@ -15,4 +15,38 @@ function handleSubmit(e) {
         alert('Escola uma moeda!');
         return;
     }
+
+    converter();
+};
+
+function converter(){
+    if(selectedCurrency.value === 'euro') {
+        valueConverted = inputValue.value / 5.32;
+        result.innerHTML = valueFormatter('pt-BR', 'EUR');
+        animateResult();
+    } else if(selectedCurrency.value === 'dol') {
+        valueConverted = inputValue.value / 4.90;
+        result.innerHTML = valueFormatter('en-US', 'USD');
+
+        animateResult();
+    }
+
+    inputValue.value = '';
+    selectedCurrency.value = '';
+};
+
+/*function valueFormatter(Locale, currency) {
+    const value = valueConverted.toLocaleString(`${Locale}`, { style: 'currency', currency: `${currency}` });
+    return`<span>💵</span> ${value}<span>💵</span>`;
+}; */
+function valueFormatter(locale, currency) {
+    const value = valueConverted.toLocaleString(locale, { style: 'currency', currency: currency });
+    return `<span>💵</span> ${value}<span>💵</span>`;
+};
+
+function animateResult() {
+    return result.animate ([
+        {transform: 'translateY(-150px)'},
+        {transform: 'translateY(0px)'},
+    ], {duration: 800});
 };
