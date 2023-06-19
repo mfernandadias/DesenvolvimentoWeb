55aaed6593b9c1ce6dd7101580330084e72814b7
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-06-19 04:21:34 -0300

    UniColadora

--- desenvolvimentoweb/aula5/js/script.js
+++ desenvolvimentoweb/aula5/js/script.js
@@ -5,14 +5,14 @@ const resultado = document.querySelector('.resultado');
 function mostrarResultado() {
     const nome = form.querySelector('.nome').value; 
     const sobrenome = form.querySelector('.sobrenome').value;
-    const Instituicao_de_ensino = form.querySelector('.Instiuicao_de_ensino').value;
+    const InstituicaoDeEnsino = form.querySelector('.Instiuicao_de_ensino').value;
     const cargo = form.querySelector('.cargo').value;
 
     const linha = document.createElement('tr');
     linha.innerHTML = `
     <td>${nome}</td>
     <td>${sobrenome}</td>
-    <td>${Instituicao_de_ensino}</td>
+    <td>${InstituicaoDeEnsino}</td>
     <td>${cargo}</td>
     `;
 
@@ -23,4 +23,4 @@ function mostrarResultado() {
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     mostrarResultado();
-});
+}); 
